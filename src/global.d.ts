810711870bc58cb5546ee4bcadc1fc87dfa98a5d
Davenchy <firon1222@gmail.com>
2020-05-03 23:38:46 +0200

    update p5 typescript support

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,13 @@
-interface Window {
-  setup: () => void;
-  draw: () => void;
+import module = require("p5");
+import * as p5Global from "p5/global";
+
+export = module;
+export as namespace module;
+
+declare global {
+  interface Window {
+    setup: () => void;
+    draw: () => void;
+    p5: typeof module;
+  }
 }

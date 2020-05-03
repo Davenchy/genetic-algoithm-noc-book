5b323b02075dc7338ce0f041eb4b5f978f4fc357
Davenchy <firon1222@gmail.com>
2020-05-03 22:34:21 +0200

    add target data type

---
+++ src/objects/target.ts
@@ -0,0 +1,25 @@
+import { Vector } from "p5";
+
+export default class Target {
+  location: Vector;
+  radius: number = 10;
+  color: number[] = [190, 0, 100];
+
+  constructor(location: Vector = null) {
+    if (location) this.location = location;
+    else {
+      const x = random(this.radius, width - this.radius);
+      const y = random(this.radius, height - this.radius);
+      this.location = createVector(x, y);
+    }
+  }
+
+  draw() {
+    const { location, color, radius: r } = this;
+    const { x, y } = location;
+
+    noStroke();
+    fill(color);
+    circle(x, y, r * 2);
+  }
+}

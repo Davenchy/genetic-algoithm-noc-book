f735b925ee02b0640f953696877807087e8f0687
Davenchy <firon1222@gmail.com>
2020-05-04 00:20:28 +0200

    write the application

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import * as p5 from "p5";
+import Population from "./objects/population";
+import Target from "./objects/target";
+
+let population: Population;
+let lifeTime: number;
+let lifeCounter: number;
+let target: Target;
+
+function setup() {
+  createCanvas(600, 600);
+  background(100);
+
+  lifeTime = 500;
+  lifeCounter = 0;
+
+  population = new Population({ target, lifeTime });
+}
+
+function draw() {
+  background(100);
+
+  if (lifeCounter < lifeTime) {
+    lifeCounter++;
+    population.live();
+  } else {
+    lifeCounter = 0;
+    population.fitness();
+    population.selection();
+    population.reproduction();
+  }
+}
+
+window.setup = setup;
+window.draw = draw;
